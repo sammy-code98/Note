9d9e950281f69a537504e76cfe9bc66adb61c129
sammy-code98 <sammysmart98@yahoo.com>
2021-10-25 15:34:29 +0100

    Feat: add redux store ~ create redux store ~ add the reducer funtion ~ connect the new note component to store ~ add the handleSubmit function to submit user input

--- src/components/NewNote.js
+++ src/components/NewNote.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
+// connect to redux store
+import { connect } from "react-redux";
 
 export class NewNote extends Component {
+  // form submission function
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // declare vars
+    const title = this.getTitle.value;
+    const message = this.getMessage.value;
+    const data = {
+      id: new Date(),
+      title,
+      message,
+      editing: false,
+    };
+    // dispatcher
+    this.props.dispatch({
+      type: "ADD_NOTE",
+      data,
+    });
+    // clear inputs after submission
+    this.getTitle.value = "";
+    this.getMessage.value = "";
+    
+
+    console.log("title  :", title, "msg : ", message);
+  };
   render() {
     return (
       <div className="container">
         <h1 className="heading">Create New Note</h1>
-        <form className="form">
-          <input type="text" required placeholder="Please Enter Note Tittle" />
+        <form className="form" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            required
+            placeholder="Please Enter Note Tittle"
+            ref={(input) => (this.getTitle = input)}
+          />
           <br />
 
           <textarea
@@ -14,6 +45,7 @@ export class NewNote extends Component {
             rows="5"
             cols="28"
             placeholder="Please Enter Note Body"
+            ref={(input) => (this.getMessage = input)}
           />
           <br />
 
@@ -23,5 +55,6 @@ export class NewNote extends Component {
     );
   }
 }
+// make sure to add the connect function
 
-export default NewNote;
+export default connect()(NewNote);
