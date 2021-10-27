5d7a18fca3a8beee045d7c5af4376cff52fe08b1
sammy-code98 <sammysmart98@yahoo.com>
2021-10-27 14:02:55 +0100

    Fix: add notecount component

--- src/components/ListNote.js
+++ src/components/ListNote.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import NoteCount from "./NoteCount";
+import { connect } from "react-redux";
 
-export class ListNote extends Component {
-    render() {
-        return (
-            <div className="container-note">
-                <h1 className="heading">All My Notes</h1>
-            </div>
-        )
-    }
+class ListNote extends Component {
+  render() {
+    return (
+      <div className="container-note">
+        <h1 className="heading">All My Notes</h1>
+        {/* get the props from the parent component and map through */}
+        {this.props.notes.map((note) => (
+          <NoteCount key={note.id} note={note} />
+        ))}
+      </div>
+    );
+  }
 }
+const mapStateToProps = (state) => {
+  return {
+    notes: state,
+  };
+};
 
-export default ListNote
+export default connect(mapStateToProps)(ListNote);
